28f0993a8f8fd329dd16ee55d1e923438d348de6
Shreyash Raj <shreyashr267@gmail.com>
2024-02-27 20:33:19 +0530

    Add routes and components for authentication and root layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Routes, Route } from "react-router-dom"
+
 import "./globals.css"
+import SigninForms from "./_auth/forms/SigninForms"
+import { Home } from "./_root/pages"
+import SignupForms from "./_auth/forms/SignupForms"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
 
 const App = () => {
-  return (
-    <div>Hello Snapgram!</div>
-  )
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForms />} />
+                    <Route path="/sign-up" element={<SignupForms />} />
+                </Route>
+                {/* private routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    )
 }
 
 export default App
