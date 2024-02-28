e67c4310fa0e1190e4749b806c44ec8c49ae3fea
Shreyash Raj <shreyashr267@gmail.com>
2024-02-28 19:18:27 +0530

    Update SignupValidation in validation/index.ts Corrected previous mistake

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,7 +1,10 @@
 import { z } from "zod"
 
 const SignupValidation = z.object({
-    username: z.string().min(2).max(50),
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
 })
 
 export { SignupValidation }
