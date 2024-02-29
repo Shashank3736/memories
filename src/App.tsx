56e7468fa9f06e85c709a03bf4b2e2fabb6bcaf1
Shreyash Raj <shreyashr267@gmail.com>
2024-02-29 13:37:46 +0530

    Add Toaster component to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Home } from "./_root/pages"
 import SignupForms from "./_auth/forms/SignupForms"
 import AuthLayout from "./_auth/AuthLayout"
 import RootLayout from "./_root/RootLayout"
+import { Toaster } from "@/components/ui/toaster"
+
 
 const App = () => {
     return (
@@ -21,6 +23,7 @@ const App = () => {
                     <Route index element={<Home />} />
                 </Route>
             </Routes>
+            <Toaster />
         </main>
     )
 }
