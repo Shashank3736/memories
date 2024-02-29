c26a2271a8f82784f02831589c9b04625bea4a43
Shreyash Raj <shreyashr267@gmail.com>
2024-03-01 00:03:41 +0530

    Update loader image path

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="flex-center w-full">
         <img 
-        src="/public/assets/icons/loader.svg" 
+        src="/assets/icons/loader.svg" 
         alt="loader"
         width={24}
         height={24} 
