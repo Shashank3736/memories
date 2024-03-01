ca1c6c73925f7be89cc1638839683ca052310bb2
Shreyash Raj <shreyashr267@gmail.com>
2024-03-01 10:30:46 +0530

    Update validation module with SigninValidation export

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,10 +1,13 @@
 import { z } from "zod"
 
-const SignupValidation = z.object({
+export const SignupValidation = z.object({
     name: z.string().min(2, { message: "Name must be at least 2 characters." }),
     username: z.string().min(2, { message: "Name must be at least 2 characters." }),
     email: z.string().email(),
     password: z.string().min(8, { message: "Password must be at least 8 characters." }),
 })
 
-export { SignupValidation }
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
+})
