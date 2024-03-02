8cb1fecaff3ca33f087e3da99908316a9fbd2017
Shreyash Raj <shreyashr267@gmail.com>
2024-03-03 01:10:05 +0530

    Update image paths in LeftSidebar and Topbar components

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -26,7 +26,7 @@ const LeftSidebar = () => {
       
       {/* Link to profile */}
       <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
-        <img src={user.imageUrl || '/assets/images/profile-placeholder.svg'} alt="profile" className='h-14 w-14 rounded-full' />
+        <img src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} alt="profile" className='h-14 w-14 rounded-full' />
         <div className="flex flex-col">
           <p className="body-bold">
             {user.name}
