0dc7308e82bb602d4b29a988d4aa65ed0823926d
Shreyash Raj <shreyashr267@gmail.com>
2024-03-04 15:51:16 +0530

    Refactor file size validation in PostValidation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -14,7 +14,7 @@ export const SigninValidation = z.object({
 
 export const PostValidation = z.object({
     caption: z.string().min(5, { message: "Caption must be at least 5 characters." }).max(2200),
-    file: z.custom<File[]>(),
+    file: z.custom<File[]>().refine((file) => file[0] && file[0].size < 8*1024*1024, { message: "File size must be less than 8MB." }),
     location: z.string().min(2, { message: "Location must be at least 2 characters." }).max(100),
     tags: z.string().min(2, { message: "Tags must be at least 2 characters." }),
 })
